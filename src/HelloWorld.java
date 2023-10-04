182dc09771fec31aeeec0267816e37b5c96631da
DESKTOP-QDP555V\fdrff <raduciobanu096@gmail.com>
2023-10-04 23:24:46 +0300

    Urmatoarea parte din Labolatorul 1.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World!");
+		 
+		int n = 10;
+		Random rand = new Random();
+		int i = rand.nextInt(n);
+		int i2 = i*2;
+		System.out.println(i2);
+		//System.out.println("Hello World!");
 
 	}
 
